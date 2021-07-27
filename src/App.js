748986fd389e9f61e3aaacc8a23b39dd369a37ab
dawon <lyumilove9@gmail.com>
2021-07-28 03:38:33 +0900

    삭제 기능, 완료 표시 기능 제작

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filtered, setFiltered] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>-Todo List-</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList/>
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+        />
+      <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Form = ({setInputText, todos, setTodos, inputText}) => {
+const Form = ({setInputText, todos, setTodos, inputText, setStatus}) => {
     const inputTextHandler = (e) => {
         console.log(e.target.value);
         setInputText(e.target.value);
@@ -10,15 +10,19 @@ const Form = ({setInputText, todos, setTodos, inputText}) => {
         setTodos([
             ...todos, {text: inputText, completed: false, id: Math.random() * 1000 }
         ]);
+        setInputText("");
     };
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    }
     return(
         <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input"/>
+            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>
             <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">전체</option>
                     <option value="completed">완료</option>
                     <option value="uncompleted">미완료</option>
